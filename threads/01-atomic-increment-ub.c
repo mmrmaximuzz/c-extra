d171e7d24e5660a5df868cbbfc190af8f4be9df6
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-01-22 23:04:38 +0300

    Add UB example with threads and counters

--- threads/01-atomic-increment-ub.c
+++ threads/01-atomic-increment-ub.c
@@ -1,5 +1,9 @@
+/* This code sample contains code with UB on non-synchronized counter updates.
+ * Please NEVER do that in your real production code. */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /* There may be problems. GCC and Clang support almost all C11 features
  * but not all C libraries do. glibc supports C11 threads starting with 2.28.
@@ -14,8 +18,34 @@
 #error "your C library does not support native C11 threads"
 #endif
 
+#define MAX_ITERATIONS 50000
+
+/* The bug is to use raw volatile objects that don't have any
+ * happened-before semanthics.
+ */
+int buggy_counter_updater(void *arg)
+{
+	volatile size_t *counter = arg;
+	for (size_t i = 0; i < MAX_ITERATIONS; ++i)
+		++(*counter);
+
+	return EXIT_SUCCESS;
+}
+
 int main()
 {
 	puts("Your C environment supports C11 threads. Congratulations!");
+
+	size_t counter = 0;
+	thrd_t thread1 = { 0 };
+	thrd_t thread2 = { 0 };
+
+	/* we ignore the error codes here for simplicity */
+	thrd_create(&thread1, buggy_counter_updater, &counter);
+	thrd_create(&thread2, buggy_counter_updater, &counter);
+	thrd_join(thread1, &(int){0});
+	thrd_join(thread2, &(int){0});
+
+	printf("the counter value is: %zu (BUGGY)\n", counter);
 	return EXIT_SUCCESS;
 }
